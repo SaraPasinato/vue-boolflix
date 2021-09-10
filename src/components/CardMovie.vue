89807be082942dd11b0e1d78d1292429e1708e77
SaraPasinato <pasinatosara.sp@gmail.com>
2021-09-10 14:43:19 +0200

    add getFleg

--- src/components/CardMovie.vue
+++ src/components/CardMovie.vue
@@ -2,7 +2,8 @@
   <div class="card" >
                <h3>{{movie.title}}</h3>
                <h4>{{movie.original_title}}</h4>
-               <p>{{movie.original_language}}</p>
+               <p v-if="getFlag()"><img :src="pathFlag" :alt="movie.original_language"></p>
+               <p v-else>{{movie.original_language}}</p>
                <p>{{movie.vote_average}}</p>
            </div>
 </template>
@@ -11,6 +12,22 @@
 export default {
  name:'CardMovie',
  props:['movie'],
+ data(){
+     return{
+         lang:['it','en'],
+         pathFlag:'',
+     }
+ },
+ methods:{
+     getFlag(){
+         if(this.lang.includes(this.movie.original_language)){
+
+           this.pathFlag= require("../assets/img/"+ this.movie.original_language+".png"); 
+         }
+          return this.pathFlag;
+
+     },
+ },
 }
 </script>
 
@@ -18,6 +35,9 @@ export default {
 
 .card{
   border:2px solid black;  
+}
 
+img{
+    width: 30px;
 }
 </style>
