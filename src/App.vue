5af4f17231ac972d57911c7f88ede0f7339c450a
SaraPasinato <pasinatosara.sp@gmail.com>
2021-09-13 11:05:59 +0200

    review endpoint text add empty validation change component name

--- src/App.vue
+++ src/App.vue
@@ -1,64 +1,71 @@
 <template>
   <div id="app">
-    <Header @searchMovie="search" />
+    <header>
+      <Search
+        @searchMovie="search"
+        placeholder="Ricerca per titolo..."
+        :btnText="btnText"
+      />
+    </header>
     <main>
-      <Gallery :movies="catalogs"/>
+      <Gallery :movies="catalogs" />
     </main>
   </div>
 </template>
 
 <script>
-import Header from "./components/Header.vue";
+import Search from "./components/Search.vue";
 import Gallery from "./components/Gallery.vue";
 import axios from "axios";
 
 export default {
   name: "App",
+  components: {
+    Search,
+    Gallery,
+  },
   data() {
     return {
-      baseUri: "https://api.themoviedb.org/3/search/",
+      baseUri: "https://api.themoviedb.org/3/",
       apiKey: "dd4f41ba1a167ca7033fd2dead8221ef",
-      movies:[],
-      series:[],
-     catalogs:[],
+      movies: [],
+      series: [],
+      catalogs: [],
+      btnText: "Cerca per Titolo",
     };
   },
   methods: {
-    search(text,type) {
-      if(type ==='all'){
-       this.getApi(text,'movie');
-       this.getApi(text,'tv');
-      }else{
-        this.catalogs=this.getApi(text,type);
+    search(text, type) {
+      // controllo stringa vuota
+      if (!text) this.movies = this.series = 0;
+
+      if (type === "all") {
+        this.getApi(text, "search/movie", "movies");
+        this.getApi(text, "search/tv", "series");
+      } else if (type === "movie") {
+        this.catalogs = this.getApi(text, "search/movie", "movies");
+      } else {
+        this.catalogs = this.getApi(text, "search/tv", "series");
       }
-       
     },
-    getApi(text,type) {
+    getApi(text, endpoint, type) {
       axios
-        .get(this.baseUri+type,{
+        .get(this.baseUri + endpoint, {
           params: {
             api_key: this.apiKey,
-            query:text,
-            language: "it-IT"
-          }
+            query: text,
+            language: "it-IT",
+          },
         })
         .then((res) => {
-          if(type==='movie'){
-            this.movies= res.data.results;
-            
-          }else{
-            this.series= res.data.results;
-          }
-          this.catalogs=[...this.movies,...this.series];
+          this[type] = res.data.results;
+
+          this.catalogs = [...this.movies, ...this.series];
         })
         .catch((err) => {
           console.log(err);
         });
     },
-  },
-  components: {
-    Header,
-    Gallery,
   },
 };
 </script>
---
+++ src/components/Search.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+      <input type="text" placeholder="" v-model.trim="currentText" @keyup.enter="$emit('searchMovie',currentText,'all')">
+      <button @click="$emit('searchMovie',currentText,'all')">{{btnText ||'Cerca'}}</button>
+  </div>
+</template>
+
+<script>
+export default {
+ name:'Search',
+ props:['placeholder','btnText'],
+ data(){
+     return{
+         currentText:'',
+     }
+ },
+}
+</script>
+
+<style>
+
+</style>
